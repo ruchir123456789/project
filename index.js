742b8c0ff539676e02cbb230ee60ad2054d50e7b
ruchir123456789 <ruchirphotos10@gmail.com>
2025-08-03 23:41:32 +0530

    What you changed

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ import { router } from "./Routes/routers.js";
 import { router as r } from "./Routes/userRoute.js";
 import cookieParser from "cookie-parser";
 import methodOverride from 'method-override';
+import path from 'path';
+import { fileURLToPath } from 'url'
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +21,11 @@ app.use(methodOverride('_method'))
 
 app.use("/", r);
 app.use("/v1/api/", router);
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set("Views", "Views");
+app.set('views', path.join(__dirname, 'Views'));
+// app.set("Views", "Views");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
