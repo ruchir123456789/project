3e0e10a63e2bb06ba7728b459817e665480224af
sagacity-ruchir-dongre <ruchirphotos10@gmail.com>
2025-10-07 23:53:16 +0530

    init

--- Middelwares/authMiddleware.js
+++ Middelwares/authMiddleware.js
@@ -21,7 +21,7 @@ const authuser = async (req, res, next) => {
 
 
 
-    //change1
+    //change2
     jwt.verify(token, SecretKey, (err, decoded) => {
       if (err) {
         return res.status(401).send({
